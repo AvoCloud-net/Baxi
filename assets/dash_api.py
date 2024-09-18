72bd3d1864a11cdda5dcfc957be68a8aaa9a6384
Red_wolf2467 <fabian.murauer@icloud.com>
2024-09-18 12:40:09 +0200

    Update dash_api.py

--- assets/dash_api.py
+++ assets/dash_api.py
@@ -1,3 +1,15 @@
+##################################
+# This is the original source code
+# of the Discord Bot's Baxi.
+#
+# When using the code (copy, change)
+# all policies and licenses must be adhered to.
+#
+# Developer: Red_Wolf2467
+# Original App: Baxi
+##################################
+
+
 from reds_simple_logger import Logger
 import configparser
 import discord
