4be661a6db14b0a4b3b3ff6bcf37ed8f0679fdc9
Red_wolf2467 <redwolf2467@pyropixle.com>
2024-09-22 21:19:46 +0200

    debug

--- assets/api/dash/save.py
+++ assets/api/dash/save.py
@@ -229,8 +229,15 @@ async def save_security_settings(request, guild: discord.Guild):
                 else:
                     block_fonts = False
 
+                channel_add_id_t = list(request_data["channels_add-activedrop"].keys())[0]
+                logger.debug.info(channel_add_id_t)
+
                 channel_add_id: int = int(list(request_data["channels_add-activedrop"].keys())[0])
                 channel_rem_id: int = int(list(request_data["channels_rem-activedrop"].keys())[0])
+
+                logger.debug.info(channel_add_id)
+                logger.debug.info(channel_rem_id)
+
                 server_index = next((index for (index, d) in enumerate(chatfilter_data) if d["guildid"] == guild.id),
                                     None)
 
