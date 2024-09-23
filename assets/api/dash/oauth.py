cb7a1744a0d36305c5fd08931cf11e91afb2f20c
Red_wolf2467 <redwolf2467@pyropixle.com>
2024-09-23 20:15:01 +0200

    Added otc based auth

--- assets/api/dash/oauth.py
+++ assets/api/dash/oauth.py
@@ -8,8 +8,7 @@
 # Developer: Red_Wolf2467
 # Original App: Baxi
 ##################################
-
-
+import pyotp
 from reds_simple_logger import Logger
 import configparser
 import discord
@@ -17,9 +16,16 @@ from cryptography.fernet import Fernet
 from quart import jsonify
 
 from assets.general.get_saves import *
+from main import encryption_key
 
 logger = Logger()
 
+def verify_one_time_code(one_time_code, secret_key):
+    totp = pyotp.TOTP(secret_key)
+    if totp.verify(one_time_code):
+        return True
+    else:
+        return False
 
 async def sync_baxi_data(request, channel: discord.TextChannel, bot):
     """
@@ -39,15 +45,18 @@ async def sync_baxi_data(request, channel: discord.TextChannel, bot):
     fernet = Fernet(auth0["FLASK"]["key"])
     CLIENT_ID = auth0["DISCORD"]["client_id"]
     REDIRECT_URI = config["DASH"]["callback_url"]
+    SECRET = auth0["FLASK"]["secret"]
 
     BOT_TOKEN = auth0["DISCORD"]["token"]
     CLIENT_SECRET = auth0["DISCORD"]["client_secret"]
 
     bot_token_bytes = BOT_TOKEN.encode('utf-8')
     client_secret_bytes = CLIENT_SECRET.encode('utf-8')
+    secret_bytes = SECRET.encode("utf-8")
 
     encrypted_token = fernet.encrypt(bot_token_bytes).decode('utf-8')  # Als String zurückgeben
     encrypted_client_secret = fernet.encrypt(client_secret_bytes).decode('utf-8')
+    encrypted_secret = fernet.encrypt(secret_bytes).decode("utf-8")
 
     logger.warn(
         f"[ 1 / 3 ]    BAXI DATA GOT PULLED WITH API KEY! Sensible data was sent in encrypted form. IP: {request.headers.get('X-Forwarded-For', request.remote_addr)}")
@@ -62,4 +71,4 @@ async def sync_baxi_data(request, channel: discord.TextChannel, bot):
     return jsonify(
         {"token": str(encrypted_token), "client_id": str(CLIENT_ID), "client_secret": str(encrypted_client_secret),
          "redirect_uri": str(REDIRECT_URI), "app_name": str(bot.user.name), "app_id": int(bot.user.id),
-         "app_verified": bool(bot.user.verified)})
+         "app_verified": bool(bot.user.verified), "secret": str(encrypted_secret)})
