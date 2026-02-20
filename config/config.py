dca25144e0a0956aa872a3dfcd6be3cf495d3408
Red_wolf2467 <support@avocloud.net>
2026-02-20 20:09:02 +0100

    fix(config): Update Discord version to 6.2002.1 for consistency

--- config/config.py
+++ config/config.py
@@ -7,7 +7,7 @@ class Discord:
     color = discord.Color.from_rgb(111, 131, 170)
     danger_color = discord.Color.from_rgb(220, 53, 69)
     warn_color = discord.Color.from_rgb(255, 193, 7)
-    version = "6 - BETAs"
+    version = "6.2002.1"
 
 
 class Web:
